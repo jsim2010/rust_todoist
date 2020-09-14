2c846380cea2d71734756a7f9e26edf36c6cfc2d
Justin Simerly <justin.simerly@endress.com>
2020-09-14 13:21:32 -0400

    Getting serde working

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,88 @@
-use std::{collections::BTreeMap, io::Read, fs::File, process::Command};
+use {
+    serde::Deserialize,
+    serde_json,
+    serde_repr::Deserialize_repr,
+    std::{collections::BTreeMap, io::Read, fs::File, process::Command},
+};
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), Failure> {
     let mut file = File::open("token")?;
     let mut token = String::new();
     file.read_to_string(&mut token)?;
     token.insert_str(0, "token=");
-    let output = Command::new("curl").args(&["https://api.todoist.com/sync/v8/sync", "-d", &token, "-d", "sync_token=*", "-d", "resource_types=[\"all\"]"]).output()?;
-    println!("{:?}", output);
+    let output = &Command::new("curl").args(&["https://api.todoist.com/sync/v8/sync", "-d", &token, "-d", "sync_token=*", "-d", "resource_types=[\"all\"]"]).output()?.stdout;
+    let sync: Sync = serde_json::from_slice(&output)?;
+    let user = User::from(sync);
+
+    for task in user.tasks {
+        println!("- {:?}", task);
+    }
+
     Ok(())
 }
 
+#[derive(Debug)]
+enum Failure {
+    Io(std::io::Error),
+    Serde(serde_json::Error),
+    Utf8(std::str::Utf8Error),
+}
+
+impl From<std::io::Error> for Failure {
+    fn from(error: std::io::Error) -> Self {
+        Self::Io(error)
+    }
+}
+
+impl From<serde_json::Error> for Failure {
+    fn from(error: serde_json::Error) -> Self {
+        Self::Serde(error)
+    }
+}
+
+impl From<std::str::Utf8Error> for Failure {
+    fn from(error: std::str::Utf8Error) -> Self {
+        Self::Utf8(error)
+    }
+}
+
+struct User {
+    tasks: Vec<Task>,
+}
+
+impl From<Sync> for User {
+    fn from(sync: Sync) -> Self {
+        let mut tasks = Vec::new();
+
+        for item in sync.items {
+            tasks.push(item.into());
+        }
+
+        Self {
+            tasks,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Task {
+    item: Item,
+}
+
+impl From<Item> for Task {
+    fn from(item: Item) -> Self {
+        Self { item }
+    }
+}
+
+#[derive(Debug, Deserialize)]
 struct Sync {
     /// A new synchronization token.
     sync_token: String,
     /// If this contains all data.
     full_sync: bool,
-    /// A [`User`].
-    user: User,
+    /// A [`UserData`].
+    user: UserData,
     /// An array of [`Project`]s.
     projects: Vec<Project>,
     /// An array of [`Item`]s.
@@ -31,24 +97,29 @@ struct Sync {
     labels: Vec<Label>,
     /// An array of [`Filter`]s.
     filters: Vec<Filter>,
-    /// Maps [`Item`] ids to their order in the daily agenda.
-    day_orders: BTreeMap<u64, u64>,
+    /// Maps items to their order in the daily agenda.
+    day_orders: BTreeMap<ItemId, Order>,
     /// An array of [`Reminder`]s.
     reminders: Vec<Reminder>,
     /// The collaborators for all shared projects.
     collaborators: Vec<Collaborator>,
     /// An array of [`CollaboratorState`]s.
+    #[serde(default)]
     collaborators_states: Vec<CollaboratorState>,
     /// An array of [`LiveNotification`]s.
     live_notifications: Vec<LiveNotification>,
     /// The id of the last [`LiveNotification`] seen by the user.
-    live_notifications_last_read_id: u64,
+    live_notifications_last_read_id: LiveNotificationId,
     /// The [`UserSettings`].
     user_settings: UserSettings,
 }
 
+#[derive(Debug, Deserialize)]
+struct Order(i64);
+
+#[derive(Debug, Deserialize)]
 /// A Todoist user.
-struct User {
+struct UserData {
     /// The default number of minutes for set automatic reminders.
     auto_reminder: u64,
     /// Link to a 195x195 image of the user's avatar.
@@ -60,7 +131,8 @@ struct User {
     /// Link to a 35x35 image of the user's avatar.
     avatar_small: String,
     /// The user's [`BusinessAccountId`].
-    business_account_id: u64,
+    #[serde(default)]
+    business_account_id: Option<BusinessAccountId>,
     /// The number of tasks set as the user's daily goal.
     daily_goal: u64,
     /// The user's desired date format.
@@ -78,17 +150,16 @@ struct User {
     /// Special internal features that apply to the user.
     features: Features,
     full_name: String,
-    id: u64,
-    image_id: String,
-    inbox_project: u64,
+    id: UserId,
+    #[serde(default)]
+    image_id: Option<String>,
+    inbox_project: ProjectId,
     is_biz_admin: bool,
     is_premium: bool,
     join_date: String,
-    karma: u64,
+    karma: f64,
     karma_trend: KarmaTrend,
     lang: Language,
-    legacy_inbox_project: u64,
-    legacy_team_inbox: u64,
     mobile_host: Option<String>,
     mobile_number: Option<String>,
     next_week: Day,
@@ -96,7 +167,8 @@ struct User {
     sort_order: SortOrder,
     start_day: Day,
     start_page: Page,
-    team_inbox: u64,
+    #[serde(default)]
+    team_inbox: Option<ProjectId>,
     theme: Theme,
     time_format: TimeFormat,
     token: String,
@@ -104,145 +176,197 @@ struct User {
     weekly_goal: u64,
 }
 
+#[derive(Debug, Deserialize)]
+struct UserId(u64);
+
+#[derive(Debug, Deserialize)]
+struct BusinessAccountId(u64);
+
+#[derive(Debug, Deserialize)]
 struct Project {
-    id: u64,
-    legacy_id: u64,
+    id: ProjectId,
     name: String,
-    color: u64,
-    parent_id: Option<u64>,
-    legacy_parent_id: Option<u64>,
-    child_order: u64,
+    color: Color,
+    parent_id: Option<ProjectId>,
+    child_order: Order,
     collapsed: Flag,
     shared: bool,
     is_deleted: Flag,
     is_archived: Flag,
     is_favorite: Flag,
-    sync_id: Option<u64>,
+    sync_id: Option<ProjectSyncId>,
+    #[serde(default)]
     inbox_project: bool,
+    #[serde(default)]
     team_inbox: bool,
 }
 
+#[derive(Debug, Deserialize)]
+struct ProjectId(u64);
+
+#[derive(Debug, Deserialize)]
+struct ProjectSyncId(u64);
+
+#[derive(Debug, Deserialize)]
 struct Item {
-    id: u64,
-    legacy_id: u64,
-    user_id: u64,
-    project_id: u64,
-    legacy_project_id: u64,
+    id: ItemId,
+    user_id: UserId,
+    project_id: ProjectId,
     content: String,
-    due: Date,
+    due: Option<Date>,
     priority: Priority,
-    parent_id: Option<u64>,
-    legacy_parent_id: Option<u64>,
-    child_order: u64,
-    section_id: Option<u64>,
-    day_order: u64,
+    parent_id: Option<ItemId>,
+    child_order: Order,
+    section_id: Option<SectionId>,
+    day_order: Order,
     collapsed: Flag,
-    labels: Vec<u64>,
-    added_by_uid: u64,
-    assigned_by_uid: u64,
-    responsible_uid: u64,
+    labels: Vec<LabelId>,
+    added_by_uid: Option<UserId>,
+    assigned_by_uid: Option<UserId>,
+    responsible_uid: Option<UserId>,
     checked: Flag,
     in_history: Flag,
     is_deleted: Flag,
-    sync_id: Option<u64>,
+    sync_id: Option<ItemSyncId>,
     date_completed: Option<String>,
     date_added: String,
 }
 
+#[derive(Debug, Deserialize)]
+struct ItemSyncId(u64);
+
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd)]
+struct ItemId(u64);
+
+#[derive(Debug, Deserialize)]
 struct Note {
-    id: u64,
-    legacy_id: u64,
-    posted_uid: u64,
-    item_id: u64,
-    legacy_item_id: u64,
-    project_id: u64,
-    legacy_project_id: u64,
+    id: NoteId,
+    posted_uid: UserId,
+    item_id: ItemId,
+    project_id: ProjectId,
     content: String,
     file_attachment: FileAttachment,
-    uids_to_notify: Vec<u64>,
+    uids_to_notify: Vec<UserId>,
     is_deleted: Flag,
     posted: String,
-    reactions: BTreeMap<String, Vec<u64>>,
+    reactions: BTreeMap<String, Vec<UserId>>,
 }
 
+#[derive(Debug, Deserialize)]
+struct NoteId(u64);
+
+#[derive(Debug, Deserialize)]
 struct ProjectNote {
-    id: u64,
-    posted_uid: u64,
-    project_id: u64,
+    id: ProjectNoteId,
+    posted_uid: UserId,
+    project_id: ProjectId,
     content: String,
     file_attachment: FileAttachment,
-    uids_to_notify: Vec<u64>,
+    uids_to_notify: Vec<UserId>,
     is_deleted: Flag,
     posted: String,
-    reactions: BTreeMap<String, Vec<u64>>,
+    reactions: BTreeMap<String, Vec<UserId>>,
 }
 
+#[derive(Debug, Deserialize)]
+struct ProjectNoteId(u64);
+
+#[derive(Debug, Deserialize)]
 struct Section {
-    id: u64,
+    id: SectionId,
     name: String,
-    project_id: u64,
-    legacy_project_id: u64,
-    section_order: u64,
+    project_id: ProjectId,
+    section_order: Order,
     collapsed: bool,
-    sync_id: Option<u64>,
+    sync_id: Option<SectionSyncId>,
     is_deleted: bool,
     is_archived: bool,
     date_archived: Option<String>,
     date_added: String,
 }
 
+#[derive(Debug, Deserialize)]
+struct SectionId(u64);
+
+#[derive(Debug, Deserialize)]
+struct SectionSyncId(u64);
+
+#[derive(Debug, Deserialize)]
 struct Label {
-    id: u64,
+    id: LabelId,
     name: String,
     color: Color,
-    item_order: u64,
+    item_order: Order,
     is_deleted: Flag,
     is_favorite: Flag,
 }
 
+#[derive(Debug, Deserialize)]
+struct LabelId(u64);
+
+#[derive(Debug, Deserialize)]
 struct Filter {
-    id: u64,
+    id: FilterId,
     name: String,
     query: String,
     color: Color,
-    item_order: u64,
+    item_order: Order,
     is_deleted: Flag,
     is_favorite: Flag,
 }
 
+#[derive(Debug, Deserialize)]
+struct FilterId(u64);
+
+#[derive(Debug, Deserialize)]
 struct Collaborator {
-    id: u64,
+    id: CollaboratorId,
     email: String,
     full_name: String,
     timezone: String,
-    image_id: u64,
+    #[serde(default)]
+    image_id: Option<String>,
 }
 
+#[derive(Debug, Deserialize)]
+struct CollaboratorId(u64);
+
+#[derive(Debug, Deserialize)]
 struct CollaboratorState {
-    project_id: u64,
-    user_id: u64,
+    project_id: ProjectId,
+    user_id: UserId,
     state: CollaboratorStatus,
     is_deleted: bool,
 }
 
+#[derive(Debug, Deserialize)]
+// Note: v8 api says there should be a `seq_no` field that holds an integer.
 struct LiveNotification {
-    id: u64,
-    legacy_id: u64,
-    created: u64,
-    from_uid: u64,
+    id: LiveNotificationId,
+    // Note: v8 api says that created should be an integer that is the epoch timestamp.
+    created: String,
+    // Note: v8 api does not say from_uid is optional.
+    #[serde(default)]
+    from_uid: Option<UserId>,
     notification_key: String,
     notification_type: String,
-    seq_no: u64,
     is_unread: Flag,
 }
 
+#[derive(Debug, Deserialize)]
+struct LiveNotificationId(u64);
+
+#[derive(Debug, Deserialize)]
 struct UserSettings {
     reminder_push: bool,
+    #[serde(default)]
     reminder_sms: bool,
     reminder_desktop: bool,
     reminder_email: bool,
 }
 
+#[derive(Debug, Deserialize_repr)]
+#[repr(u8)]
 enum Color {
     Crimson = 30,
     Red = 31,
@@ -266,11 +390,13 @@ enum Color {
     Tan = 49,
 }
 
+#[derive(Debug, Deserialize)]
 enum CollaboratorStatus {
     Active,
     Invited,
 }
 
+#[derive(Debug, Deserialize)]
 struct FileAttachment {
     file_type: String,
     file_name: String,
@@ -279,11 +405,14 @@ struct FileAttachment {
     upload_state: UploadState,
 }
 
+#[derive(Debug, Deserialize)]
 enum UploadState {
     Pending,
     Completed,
 }
 
+#[derive(Debug, Deserialize_repr)]
+#[repr(u8)]
 enum Priority {
     Natural = 1,
     High = 2,
@@ -291,6 +420,7 @@ enum Priority {
     VeryUrgent = 4,
 }
 
+#[derive(Debug, Deserialize)]
 struct Date {
     date: String,
     timezone: Option<String>,
@@ -299,19 +429,24 @@ struct Date {
     is_recurring: bool,
 }
 
+#[derive(Debug, Deserialize)]
 struct TimezoneInfo {
     gmt_string: String,
-    hours: u64,
-    is_dst: u64,
-    minutes: u64,
+    hours: i8,
+    is_dst: Flag,
+    minutes: u8,
     timezone: String,
 }
 
+#[derive(Debug, Deserialize_repr)]
+#[repr(u8)]
 enum TimeFormat {
     TwentyFour = 0,
     Twelve = 1,
 }
 
+#[derive(Debug, Deserialize_repr)]
+#[repr(u8)]
 enum Theme {
     Theme0 = 0,
     Theme1 = 1,
@@ -326,28 +461,35 @@ enum Theme {
     Theme10 = 10,
 }
 
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
 enum Page {
     InfoPage,
     Blank,
-    Project(String),
     Query(String),
 }
 
+#[derive(Debug, Deserialize_repr)]
+#[repr(u8)]
 enum SortOrder {
     OldestDatesFirst = 0,
     OldestDatesLast = 1,
 }
 
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
 enum KarmaTrend {
     Up,
 }
 
+#[derive(Debug, Deserialize)]
 struct Features {
     /// If the user has enabled beta.
     beta: Flag,
     /// If inline date parsing is enabled.
-    datist_inline_disabled: bool,
-    datist_lang: Option<Language>,
+    dateist_inline_disabled: bool,
+    dateist_lang: Option<Language>,
+    #[serde(default)]
     gold_theme: bool,
     has_push_reminders: bool,
     karma_disabled: bool,
@@ -355,11 +497,15 @@ struct Features {
     restriction: u64,
 }
 
+#[derive(Debug, Deserialize_repr)]
+#[repr(u8)]
 enum Flag {
     False = 0,
     True = 1,
 }
 
+#[derive(Debug, Deserialize_repr)]
+#[repr(u8)]
 enum Day {
     Monday = 1,
     Tuesday = 2,
@@ -370,6 +516,8 @@ enum Day {
     Sunday = 7,
 }
 
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
 enum Reminder {
     Email,
     Mobile,
@@ -378,6 +526,8 @@ enum Reminder {
 }
 
 /// The format of a date.
+#[derive(Debug, Deserialize_repr)]
+#[repr(u8)]
 enum DateFormat {
     /// dd-mm-yyyy
     DayMonth = 0,
@@ -385,39 +535,40 @@ enum DateFormat {
     MonthDay = 1,
 }
 
+#[derive(Debug, Deserialize)]
 enum Language {
-    /// da
+    #[serde(rename = "da")]
     Danish,
-    /// ge
+    #[serde(rename = "ge")]
     German,
-    /// en
+    #[serde(rename = "en")]
     English,
-    /// es
+    #[serde(rename = "es")]
     Spanish,
-    /// fi
+    #[serde(rename = "fi")]
     Finnish,
-    /// fr
+    #[serde(rename = "fr")]
     French,
-    /// it
+    #[serde(rename = "it")]
     Italian,
-    /// ja
+    #[serde(rename = "ja")]
     Japanese,
-    /// ko
+    #[serde(rename = "ko")]
     Korean,
-    /// nl
+    #[serde(rename = "nl")]
     Dutch,
-    /// pl
+    #[serde(rename = "pl")]
     Polish,
-    /// pt_BR
+    #[serde(rename = "pt_Br")]
     BrazilianPortuguese,
-    /// ru
+    #[serde(rename = "ru")]
     Russian,
-    /// sv
+    #[serde(rename = "sv")]
     Sweedish,
-    /// tr
+    #[serde(rename = "tr")]
     Turkish,
-    /// zh_CN
+    #[serde(rename = "zh_CN")]
     MainlandChinese,
-    /// zh_TW
+    #[serde(rename = "zh_TW")]
     TaiwanChinese,
 }
